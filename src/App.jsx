a6bb62bb19e7a41897d24a201910c3bbcc068aae
Christian <mail@christianhohenbild.de>
2024-05-22 18:08:25 +0200

    setup basic scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 import { Canvas } from "@react-three/fiber"
-import { Environment } from "@react-three/drei"
+import { Environment, OrbitControls } from "@react-three/drei"
 
-import './index.css'
+import "./index.css"
 
-import Experience from "./Experience"
-
+import Model from "./Model"
 
 export default function App() {
-
- return (
-
-  
-    <Canvas shadows camera={{ position: [0, 0, 4], fov: 40 }}>
-      <Environment
-        files="./textures/envmap.hdr" />
-        <color 
-          attach="background" 
-          args={["#eeeeee"]} />
-      <Experience />
+  return (
+    <Canvas shadows camera={{ position: [0, 0, 4], fov: 50 }}>
+      <Environment files="./textures/envmap.hdr" />
+      <color attach="background" args={["#eeeeee"]} />
+      <OrbitControls />
+      <directionalLight
+        position={[-5, 5, 5]}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <Model />
+      <mesh
+        rotation={[-0.5 * Math.PI, 0, 0]}
+        position={[0, -0.81, 0]}
+        receiveShadow
+      >
+        <planeGeometry args={[10, 10, 1, 1]} />
+        <shadowMaterial transparent opacity={0.2} />
+      </mesh>
     </Canvas>
-  
-  );
+  )
 }
-
